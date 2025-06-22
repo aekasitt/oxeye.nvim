ba4f91b0a5a3548f2d15f999956d86a1d09731cc
Sitt Guruvanich <aekazitt@gmail.com>
2025-06-22 23:43:48 +0700

    chore(init): avoid nvim_echo in fast event context

--- plugin/oxeye.lua
+++ plugin/oxeye.lua
@@ -4,5 +4,7 @@ local has_fzf_lua, _ = pcall(require, 'fzf-lua')
 local has_plenary, _ = pcall(require, 'plenary')
 
 if not has_fzf_lua or not has_plenary then
-  error('Unmet dependencies for oxeye.nvim')
+  vim.schedule(function()
+    vim.notify('Unmet dependencies for oxeye.nvim', vim.log.levels.ERROR)
+  end)
 end
