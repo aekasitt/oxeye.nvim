4decd2541d8a6a36fb1538a07512df62e4c21f9a
Sitt Guruvanich <aekazitt@gmail.com>
2025-06-22 23:42:20 +0700

    feat(build): handle installation lifecycle logs

--- plugin/build.lua
+++ plugin/build.lua
@@ -2,14 +2,41 @@
 
 local Job = require('plenary.job')
 
-Job:new({
-  args = { 'install', 'ast-grep', '--locked' },
-  command = 'cargo',
-  on_exit = function(_, build_return_val)
-    if build_return_val ~= 0 then
-      error('Unable to install ast-grep required for plugin')
-    else
-      print('Successfully installed ast-grep')
-    end
-  end,
-}):start()
+if vim.fn.executable('sg') == 0 then
+  vim.schedule(function()
+    vim.notify('Installing ast-grep for oxeye.nvim…', vim.log.levels.INFO)
+  end)
+  Job:new({
+    args = { 'install', 'ast-grep', '--locked' },
+    command = 'cargo',
+    on_stdout = function(_, data)
+      if data then
+        if not data:match("^%s*Updating crates.io index") and not data:match("^%s*Compiling") then
+          vim.schedule(function()
+            vim.notify(data, vim.log.levels.INFO)
+          end)
+        end
+      end
+    end,
+    on_stderr = function(_, data)
+      if data then
+        if not data:match("^%s*Updating crates.io index") and not data:match("^%s*Compiling") then
+          vim.schedule(function()
+            vim.notify(data, vim.log.levels.INFO)
+          end)
+        end
+      end
+    end,
+    on_exit = function(_, return_val)
+      if return_val ~= 0 then
+        vim.schedule(function()
+          vim.notify('Unable to install ast-grep required for plugin', vim.log.levels.ERROR)
+        end)
+      else
+        vim.schedule(function()
+          vim.notify('Successfully installed ast-grep', vim.log.levels.SUCCESS)
+        end)
+      end
+    end,
+  }):start()
+end
